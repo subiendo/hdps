c903241f58f6a695bc9c2d9d21755886e3b17183
huracan <huracan@192.168.1.136>
2014-03-31 19:47:31 +0100

    Refactored replacing switch and if inside classes

--- test/expevaluator/ExpEvaluatorTest.java
+++ test/expevaluator/ExpEvaluatorTest.java
@@ -8,6 +8,7 @@ public class ExpEvaluatorTest {
     @Test
     public void testMain() {
         Assert.assertEquals(1.0, new Constant(1.0).evaluate());
-        Assert.assertEquals(6.0, new Operation('*', new Constant(2.0), new Constant(3.0)).evaluate());
+        Assert.assertEquals(6.0, new Addition(new Constant(3.0), new Constant(3.0)).evaluate());
+        Assert.assertEquals(9.0, new Multiplication(new Constant(3.0), new Constant(3.0)).evaluate());
     }
 }
