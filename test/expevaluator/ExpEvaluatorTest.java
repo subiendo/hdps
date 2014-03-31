02864eb3bba4c74fb520bbe883f058fc2ba1b0d0
huracan <huracan@192.168.1.136>
2014-03-31 19:04:26 +0100

    green state of refactoring switch with polymorphism

--- test/expevaluator/ExpEvaluatorTest.java
+++ test/expevaluator/ExpEvaluatorTest.java
@@ -7,7 +7,7 @@ public class ExpEvaluatorTest {
     
     @Test
     public void testMain() {
-        Assert.assertEquals(1.0, new Expression('#',1.0).evaluate());
-        Assert.assertEquals(6.0, new Expression('*', new Expression('#',2.0), new Expression('#',3.0)).evaluate());
+        Assert.assertEquals(1.0, new Constant(1.0).evaluate());
+        Assert.assertEquals(6.0, new Operation('*', new Constant(2.0), new Constant(3.0)).evaluate());
     }
 }
